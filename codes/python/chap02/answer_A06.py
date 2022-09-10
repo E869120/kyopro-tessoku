d8f151fa3914759e47b218b143cd15a23d73d8f3
Masataka Yoneda <e869120@gmail.com>
2022-09-10 15:53:56 +0900

    Create answer_A06.py

---
+++ codes/python/chap02/answer_A06.py
@@ -0,0 +1,14 @@
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+L = [ None ] * Q
+R = [ None ] * Q
+for j in range(Q):
+	L[j], R[j] = map(int, input().split())
+
+S = [ None ] * (N + 1)
+S[0] = 0
+for i in range(N):
+	S[i + 1] = S[i] + A[i]
+
+for j in range(Q):
+	print(B[R[j]] - B[L[j] - 1])
