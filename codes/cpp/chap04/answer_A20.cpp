e38867c070bfda88e8da3ea34cf899bc868fb9f3
Masataka Yoneda <e869120@gmail.com>
2022-09-10 15:08:20 +0900

    Create answer_A20.cpp

---
+++ codes/cpp/chap04/answer_A20.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int N, M, dp[2009][2009];
+string S, T;
+
+int main() {
+	// 入力
+	cin >> S; N = S.size();
+	cin >> T; M = T.size();
+
+	// 動的計画法
+	dp[0][0] = 0;
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= M; j++) {
+			if (i >= 1 && j >= 1 && S[i - 1] == T[j - 1]) {
+				dp[i][j] = max({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] + 1 });
+			}
+			else if (i >= 1 && j >= 1) {
+				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+			}
+			else if (i >= 1) {
+				dp[i][j] = dp[i - 1][j];
+			}
+			else if (j >= 1) {
+				dp[i][j] = dp[i][j - 1];
+			}
+		}
+	}
+
+	// 出力
+	cout << dp[N][M] << endl;
+	return 0;
+}
