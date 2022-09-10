4cee7667a9ab7887adb20b58c94fc460c5de5160
Masataka Yoneda <e869120@gmail.com>
2022-09-10 15:09:03 +0900

    Create answer_A22.cpp

---
+++ codes/cpp/chap04/answer_A22.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, A[100009], B[100009], dp[100009];
+
+int main() {
+	// 入力
+	cin >> N;
+	for (int i = 1; i <= N - 1; i++) cin >> A[i];
+	for (int i = 1; i <= N - 1; i++) cin >> B[i];
+
+	// 配列の初期化
+	for (int i = 1; i <= N; i++) dp[i] = 0;
+
+	// 動的計画法 → 出力
+	for (int i = 1; i <= N - 1; i++) {
+		dp[A[i]] = max(dp[A[i]], dp[i] + 100);
+		dp[B[i]] = max(dp[B[i]], dp[i] + 150);
+	}
+	cout << dp[N] << endl;
+	return 0;
+}
