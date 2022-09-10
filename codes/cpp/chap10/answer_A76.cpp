5717833fbc3d27c1a292e86ab42fa57b9af9aa48
Masataka Yoneda <e869120@gmail.com>
2022-09-10 15:51:13 +0900

    Create answer_A76.cpp

---
+++ codes/cpp/chap10/answer_A76.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const long long mod = 1000000007;
+long long N, W, L, R, X[150009];
+int dp[150009], sum[150009];
+
+int main() {
+	// 入力
+	cin >> N >> W >> L >> R;
+	for (int i = 1; i <= N; i++) cin >> X[i];
+
+	// 西岸を足場 0、東岸を足場 N+1 とみなす
+	X[0] = 0; X[N + 1] = W;
+
+	// 動的計画法・出力
+	dp[0] = 1; sum[0] = 1;
+	for (int i = 1; i <= N + 1; i++) {
+		int posL = lower_bound(X, X + N + 2, X[i] - R) - X;
+		int posR = lower_bound(X, X + N + 2, X[i] - L + 1) - X; posR--;
+
+		// dp[i] の値を累積和で計算（引き算の余りに注意！）
+		dp[i] = sum[posR];
+		if (posL >= 1) dp[i] -= sum[posL - 1];
+		dp[i] = (dp[i] + mod) % mod;
+
+		// 累積和 sum[i] を更新
+		sum[i] = sum[i - 1] + dp[i];
+		sum[i] %= mod;
+	}
+	cout << dp[N + 1] << endl;
+	return 0;
+}
