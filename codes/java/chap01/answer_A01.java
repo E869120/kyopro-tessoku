9028658fd90e319067278003bbf48ea868ff463a
Masataka Yoneda <e869120@gmail.com>
2022-09-10 20:22:43 +0900

    Create answer_A01.java

---
+++ codes/java/chap01/answer_A01.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); // 入力部分
+		System.out.println(N * N); // 出力部分
+	}
+};
