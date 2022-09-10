01915281e6881e421268186780d3eb3295af9d92
Masataka Yoneda <e869120@gmail.com>
2022-09-10 15:11:48 +0900

    Create answer_A28.cpp

---
+++ codes/cpp/chap05/answer_A28.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// 入力
+	long long N, A[100009]; char T[100009];
+	cin >> N;
+	for (int i = 1; i <= N; i++) cin >> T[i] >> A[i];
+
+	// 出力（Answer は現在の黒板の数）
+	long long Answer = 0;
+	for (int i = 1; i <= N; i++) {
+		if (T[i] == '+') Answer += A[i];
+		if (T[i] == '-') Answer -= A[i];
+		if (T[i] == '*') Answer *= A[i];
+
+		// 引き算で答えが 0 未満になった場合
+		if (Answer < 0) Answer += 10000;
+
+		// ここで余りをとっている！
+		Answer %= 10000;
+		cout << Answer << endl;
+	}
+	return 0;
+}
