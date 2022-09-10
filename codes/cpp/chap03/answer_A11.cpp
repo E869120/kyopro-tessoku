4b42d01d0273a78eaefa433ca3d9a9f485fa5c42
Masataka Yoneda <e869120@gmail.com>
2022-09-10 15:03:09 +0900

    Update answer_A11.cpp

--- codes/cpp/chap03/answer_A11.cpp
+++ codes/cpp/chap03/answer_A11.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
+
+int N, X, A[100009];
+
+// 整数 x が何番目に存在するかを返す
+int search(int x) {
+	int L = 1, R = N;
+	while (L <= R) { // 探索範囲がなくなるまで、比較を続ける
+		int M = (L + R) / 2;
+		if (x < A[M]) R = M - 1;
+		if (x == A[M]) return M;
+		if (x > A[M]) L = M + 1;
+	}
+	return -1; // 整数 x が存在しない（注：この問題の制約で -1 が返されることはない）
+}
 
 int main() {
 	// 入力
-	int N, A[100009];
-	cin >> N;
+	cin >> N >> X;
 	for (int i = 1; i <= N; i++) cin >> A[i];
 
-	// 配列のソート
-	// ここで、関数 sort(A+L, A+R) では A[L] から A[R-1] までの部分をソートする
-	sort(A + 1, A + N + 1);
-
-	// 出力
-	for (int i = 1; i <= N; i++) cout << A[i] << endl;
+	// 二分探索を行う
+	int Answer = search(X);
+	cout << Answer << endl;
 	return 0;
 }
