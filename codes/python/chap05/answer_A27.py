b9ecc527504e07d59d13dfd007329b37deed7c14
Masataka Yoneda <e869120@gmail.com>
2022-09-10 16:44:13 +0900

    Create answer_A27.py

---
+++ codes/python/chap05/answer_A27.py
@@ -0,0 +1,13 @@
+def GCD(A, B):
+	while A >= 1 and B >= 1:
+		if A >= B:
+			A = A % B # A の値を変更する場合
+		else:
+			B = B % A # B の値を変更する場合
+	if A >= 1:
+		return A
+	return B
+
+# 入力
+A, B = map(int, input().split())
+print(GCD(A, B))
