4ea6e07094fb267874496b78049a37936a2b1922
Masataka Yoneda <e869120@gmail.com>
2022-09-12 02:09:54 +0900

    Create answer_B26.cpp

---
+++ editorial/chap05/answer_B26.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int N;
+bool Deleted[1000009]; // 整数 x が消されている場合に限り Deleted[x]=true
+
+int main() {
+	// 入力
+	cin >> N;
+
+	// エラトステネスのふるい（i は √N 以下の最大の整数までループする）
+	for (int i = 2; i <= N; i++) Deleted[i] = false;
+	for (int i = 2; i * i <= N; i++) {
+		if (Deleted[i] == true) continue;
+		for (int j = i * 2; j <= N; j += i) Deleted[j] = true;
+	}
+
+	// 答えを求める
+	int Answer = 0;
+	for (int i = 2; i <= N; i++) {
+		if (Deleted[i] == false) Answer += 1;
+	}
+	cout << Answer << endl;
+	return 0;
+}
