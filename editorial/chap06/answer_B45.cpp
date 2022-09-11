07dddc88723d54a0cb6b8bc72bb2a7f4c3f85c53
Masataka Yoneda <e869120@gmail.com>
2022-09-12 01:18:47 +0900

    Create answer_B45.cpp

---
+++ editorial/chap06/answer_B45.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ 
+long long a, b, c;
+ 
+int main() {
+	// 入力
+	cin >> a >> b >> c;
+ 
+	// 出力
+	if (a + b + c == 0) cout << "Yes" << endl;
+	else cout << "No" << endl;
+	return 0;
+}
