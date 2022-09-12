4cbc57c7b70d61c40ce04abdd76cfa5800280300
Masataka Yoneda <e869120@gmail.com>
2022-09-12 21:19:13 +0900

    Create answer_A22.java

---
+++ codes/java/chap04/answer_A22.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 入力（文字列は 0 文字目から始まることに注意！）
+		int N = sc.nextInt();
+		int[] A = new int[N + 1];
+		int[] B = new int[N + 1];
+		for (int i = 1; i <= N - 1; i++) A[i] = sc.nextInt();
+		for (int i = 1; i <= N - 1; i++) B[i] = sc.nextInt();
+
+		// 配列の初期化
+		int[] dp = new int[N + 1];
+		for (int i = 1; i <= N; i++) dp[i] = 0;
+
+		// 動的計画法
+		for (int i = 1; i <= N - 1; i++) {
+			dp[A[i]] = Math.max(dp[A[i]], dp[i] + 100);
+			dp[B[i]] = Math.max(dp[B[i]], dp[i] + 150);
+		}
+
+		// 出力
+		System.out.println(dp[N]);
+	}
+};
