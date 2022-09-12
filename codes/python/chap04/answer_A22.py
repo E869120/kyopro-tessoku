fab747c54ec961a78e728edcab75f4c4fe0fe2ea
Masataka Yoneda <e869120@gmail.com>
2022-09-12 11:43:36 +0900

    Create answer_A22.py

---
+++ codes/python/chap04/answer_A22.py
@@ -0,0 +1,15 @@
+# 入力（配列 A, B は 0 番目から始まることに注意！）
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+# 配列の初期化
+dp = [ 0 ] * (N + 1)
+
+# 動的計画法
+for i in range(1, N):
+	dp[A[i-1]] = max(dp[A[i-1]], dp[i] + 100)
+	dp[B[i-1]] = max(dp[B[i-1]], dp[i] + 150)
+
+# 出力
+print(dp[N])
