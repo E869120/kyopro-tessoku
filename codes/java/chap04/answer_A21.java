7d8fa0a81d8753876c0474e3711060d46f9ab4ce
Masataka Yoneda <e869120@gmail.com>
2022-09-12 21:20:48 +0900

    Update answer_A21.java

--- codes/java/chap04/answer_A21.java
+++ codes/java/chap04/answer_A21.java
@@ -22,11 +22,11 @@ class Main {
 
 				// score1 の値（l-1 番目のブロックを取り除くときの得点）を求める
 				int score1 = 0;
-				if (l <= P[l - 1] && P[l - 1] <= r) score1 = A[l - 1];
+				if (l >= 2 && l <= P[l - 1] && P[l - 1] <= r) score1 = A[l - 1];
 
 				// score2 の値（r+1 番目のブロックを取り除くときの得点）を求める
 				int score2 = 0;
-				if (l <= P[r + 1] && P[r + 1] <= r) score2 = A[r + 1];
+				if (r < N && l <= P[r + 1] && P[r + 1] <= r) score2 = A[r + 1];
 
 				// dp[l][r] を求める
 				if (l == 1) {
@@ -36,14 +36,14 @@ class Main {
 					dp[l][r] = dp[l - 1][r] + score1;
 				}
 				else {
-					dp[l][r] = max(dp[l - 1][r] + score1, dp[l][r + 1] + score2);
+					dp[l][r] = Math.max(dp[l - 1][r] + score1, dp[l][r + 1] + score2);
 				}
 			}
 		}
 
 		// 出力
 		int Answer = 0;
-		for (int i = 1; i <= N; i++) Answer = max(Answer, dp[i][i]);
+		for (int i = 1; i <= N; i++) Answer = Math.max(Answer, dp[i][i]);
 		System.out.println(Answer);
 	}
 };
