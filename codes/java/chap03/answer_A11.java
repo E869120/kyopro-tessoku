8bf155604b8d8c25408c916d868c138bf06a8683
Masataka Yoneda <e869120@gmail.com>
2022-09-12 21:49:29 +0900

    Create answer_A11.java

---
+++ codes/java/chap03/answer_A11.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		// 入力
+		N = sc.nextInt();
+		X = sc.nextInt();
+		A = new int[N + 1];
+		for (int i = 1; i <= N; i++) A[i] = sc.nextInt();
+
+		// 二分探索を行う
+		int Answer = search(X);
+		System.out.println(Answer);
+	}
+
+	// 整数 x が何番目に存在するかを返す
+	static int N, X;
+	static int[] A;
+	static int search(int x) {
+		int L = 1, R = N;
+		while (L <= R) { // 探索範囲がなくなるまで、比較を続ける
+			int M = (L + R) / 2;
+			if (x < A[M]) R = M - 1;
+			if (x == A[M]) return M;
+			if (x > A[M]) L = M + 1;
+		}
+		return -1; // 整数 x が存在しない（注：この問題の制約で -1 が返されることはない）
+	}
+};
