babb5f2eaf219237f62cd69db7166b51107cb6b6
Masataka Yoneda <e869120@gmail.com>
2022-09-14 23:13:12 +0900

    Update answer_B22.cpp

--- editorial/chap04/answer_B22.cpp
+++ editorial/chap04/answer_B22.cpp
@@ -3,13 +3,14 @@
 #include <algorithm>
 using namespace std;
 
-int N, H[100009];
+int N, A[100009], B[100009];
 int dp[100009];
 
 int main() {
 	// 入力
 	cin >> N;
-	for (int i = 1; i <= N; i++) cin >> H[i];
+	for (int i = 2; i <= N; i++) cin >> A[i];
+	for (int i = 3; i <= N; i++) cin >> B[i];
 
 	// 配列 dp の初期化
 	dp[1] = 0;
@@ -17,8 +18,8 @@ int main() {
 
 	// 動的計画法
 	for (int i = 1; i <= N; i++) {
-		if (i <= N - 1) dp[i + 1] = min(dp[i + 1], dp[i] + abs(H[i + 1] - H[i])); // 足場 i+1 に行く場合
-		if (i <= N - 2) dp[i + 2] = min(dp[i + 2], dp[i] + abs(H[i + 2] - H[i])); // 足場 i+2 に行く場合
+		if (i <= N - 1) dp[i + 1] = min(dp[i + 1], dp[i] + A[i + 1]); // 部屋 i+1 に行く場合
+		if (i <= N - 2) dp[i + 2] = min(dp[i + 2], dp[i] + B[i + 2]); // 部屋 i+2 に行く場合
 	}
 
 	// 出力
