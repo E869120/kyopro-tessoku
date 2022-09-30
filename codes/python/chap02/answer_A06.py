76fa4e69a7262d4b47d5136c6373cbe955d0c335
Masataka Yoneda <e869120@gmail.com>
2022-09-30 16:12:54 +0900

    Update answer_A06.py

--- codes/python/chap02/answer_A06.py
+++ codes/python/chap02/answer_A06.py
@@ -4,11 +4,11 @@ L = [ None ] * Q
 R = [ None ] * Q
 for j in range(Q):
 	L[j], R[j] = map(int, input().split())
-
+ 
 S = [ None ] * (N + 1)
 S[0] = 0
 for i in range(N):
 	S[i + 1] = S[i] + A[i]
-
+ 
 for j in range(Q):
-	print(B[R[j]] - B[L[j] - 1])
+	print(S[R[j]] - S[L[j] - 1])
