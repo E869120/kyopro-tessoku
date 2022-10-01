7456e5956ffca2ae59043acacf244ad36100e2aa
Masataka Yoneda <e869120@gmail.com>
2022-10-01 17:50:50 +0900

    Create answer_A49_greedy1_37454pts.py

---
+++ codes/python/chap07/answer_A49_greedy1_37454pts.py
@@ -0,0 +1,53 @@
+
+# 入力
+T = int(input())
+P = [ None ] * T
+Q = [ None ] * T
+R = [ None ] * T
+for i in range(T):
+	P[i], Q[i], R[i] = map(int, input().split())
+	P[i] -= 1 # 0 始まりに直す
+	Q[i] -= 1
+	R[i] -= 1
+
+# 配列 A の初期化
+A = [ 0 ] * 20
+
+# 貪欲法
+CurrentScore = 0
+for i in range(T):
+	# パターン A の場合のスコアを求める
+	ScoreA = CurrentScore;
+	PatA = [ 0 ] * 20
+	for j in range(20):
+		PatA[j] = A[j]
+	PatA[P[i]] += 1
+	PatA[Q[i]] += 1
+	PatA[R[i]] += 1
+	for j in range(20):
+		if PatA[j] == 0:
+			ScoreA += 1
+
+	# パターン B の場合のスコアを求める
+	ScoreB = CurrentScore;
+	PatB = [ 0 ] * 20
+	for j in range(20):
+		PatB[j] = A[j]
+	PatB[P[i]] -= 1
+	PatB[Q[i]] -= 1
+	PatB[R[i]] -= 1
+	for j in range(20):
+		if PatB[j] == 0:
+			ScoreB += 1
+
+	# スコアの大きい方を採用
+	if ScoreA >= ScoreB:
+		print("A")
+		CurrentScore = ScoreA
+		for j in range(20):
+			A[j] = PatA[j]
+	else:
+		print("B")
+		CurrentScore = ScoreB
+		for j in range(20):
+			A[j] = PatB[j]
